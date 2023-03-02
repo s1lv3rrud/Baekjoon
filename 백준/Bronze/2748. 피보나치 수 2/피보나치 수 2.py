4c853a317cfa26cb5efee00386c56fb0f565d9bd
s1lv3rrud <86763857+s1lv3rrud@users.noreply.github.com>
2023-03-02 21:54:45 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+fibo = [-1 for _ in range(91)]
+fibo[0] = 0
+fibo[1] = 1
+
+def solve(n):
+    if fibo[n] > -1:
+        return fibo[n]
+    else:
+        fibo[n] = solve(n-1) + solve(n-2)
+    
+    return fibo[n]
+
+n = int(input())
+print(solve(n))

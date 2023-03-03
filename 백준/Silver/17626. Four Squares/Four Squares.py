e26cfcae5610d1fc4cf6df372e275b042732d38f
s1lv3rrud <86763857+s1lv3rrud@users.noreply.github.com>
2023-03-04 02:37:07 +0900

    [Silver III] Title: Four Squares, Time: 136 ms, Memory: 114720 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -3,13 +3,15 @@ input = sys.stdin.readline
 
 n = int(input())
 
-sqrt = [0, 1]
+sqrt = [-1] * (n+1)
+sqrt[0], sqrt[1] = 0, 1
 for i in range(2, n+1):
-    minnum = i
-    j = 1
-    while j*j <= i:
-        minnum = min(minnum, sqrt[i-j*j])
-        j += 1
-    sqrt.append(minnum + 1)
+    if(sqrt[i] == -1):
+        minnum = i
+        j = 1
+        while j*j <= i:
+            minnum = min(minnum, sqrt[i-j*j])
+            j += 1
+        sqrt[i] = minnum + 1
 
 print(sqrt[n])

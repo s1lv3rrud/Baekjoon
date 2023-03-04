65532df6c09f1f716865c6547aa41435ef76f531
s1lv3rrud <86763857+s1lv3rrud@users.noreply.github.com>
2023-03-04 13:47:55 +0900

    [Silver V] Title: 집합, Time: 3692 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -3,25 +3,25 @@ input = sys.stdin.readline
 
 m = int(input())
 
-s = set()
-for i in range(m):
+bit = 0
+for _ in range(m):
     order = input().split()
-    if order[0] == "add":
-        s.add(int(order[1]))
-    elif order[0] == "remove":
-        if int(order[1]) in s:
-            s.remove(int(order[1]))
-    elif order[0] == "check":
-        if int(order[1]) in s:
-            print(1)
-        else:
-            print(0)
-    elif order[0] == "toggle":
-        if int(order[1]) in s:
-            s.remove(int(order[1]))
-        else:
-            s.add(int(order[1]))
-    elif order[0] == "all":
-        s = set([i for i in range(1, 21)])
+
+    if order[0] == "all":
+        bit = (1 << 20) - 1
     elif order[0] == "empty":
-        s.clear()
+        bit = 0
+    else:
+        num = int(order[1]) - 1
+        if order[0] == 'add':
+            bit |= (1 << num)
+        elif order[0] == 'remove':
+            bit &= ~(1 << num)
+        elif order[0] == 'check':
+            if bit & (1 << num) == 0:
+                print(0)
+            else:
+                print(1)
+        elif order[0] == 'toggle':
+            # XOR
+            bit = bit ^ (1 << num)

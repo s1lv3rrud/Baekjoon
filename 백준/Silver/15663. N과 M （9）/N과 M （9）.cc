8fc2d494a8a2edb0364ea1f36141c96ee2c00077
s1lv3rrud <86763857+s1lv3rrud@users.noreply.github.com>
2024-01-16 01:11:50 +0900

    [Silver II] Title: N과 M (9), Time: 8 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+vector<int> input(9);
+vector<int> check(9);
+vector<int> print(9);
+
+void backTracking(int index) {
+    // 탐색 종료 조건
+    if (index == m) {
+        for (int i = 0; i < m; i++) {
+            cout << print[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    int pre = 0;
+    for (int i = 1; i <= n; i++) {
+        if (!check[i] && input[i] != pre) {
+            print[index] = input[i];
+            pre = print[index];
+            check[i] = true;
+            backTracking(index + 1);
+            check[i] = false;
+        }
+    }
+}
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        cin >> input[i];
+    }
+    sort(input.begin(), input.begin() + n + 1);
+
+    backTracking(0);
+
+    return 0;
+}

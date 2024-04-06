ef7e150e8c5975924f0bb9d8e0d956fa12c5e197
s1lv3rrud <86763857+s1lv3rrud@users.noreply.github.com>
2024-04-07 00:10:04 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << 197 << "\ns1lv3rrud";
+
+    return 0;
+}

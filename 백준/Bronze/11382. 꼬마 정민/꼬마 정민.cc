fe45049e1eebae6b87d58ecbcf2262f0435e3799
s1lv3rrud <86763857+s1lv3rrud@users.noreply.github.com>
2024-04-09 03:19:16 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    long a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c;
+
+    return 0;
+}

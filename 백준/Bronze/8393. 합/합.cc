10ccc15bea49d5b79d9ae2a25fabac52e3446918
s1lv3rrud <86763857+s1lv3rrud@users.noreply.github.com>
2024-05-09 02:30:09 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, sum = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    cout << sum;
+    return 0;
+}

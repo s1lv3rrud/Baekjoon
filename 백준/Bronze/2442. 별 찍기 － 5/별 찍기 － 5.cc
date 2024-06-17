c758e5f22d7c38abe2c688c95d6c3718d2847ece
s1lv3rrud <86763857+s1lv3rrud@users.noreply.github.com>
2024-06-18 00:19:36 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			cout << " ";
+		}
+		for (int j = 0; j < 2*i + 1; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+    return 0;
+}

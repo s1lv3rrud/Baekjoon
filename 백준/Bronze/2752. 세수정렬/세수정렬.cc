28170b515a87e61087e0161cd21f77607fbcf746
s1lv3rrud <86763857+s1lv3rrud@users.noreply.github.com>
2024-07-02 00:45:28 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(void)
+{
+    vector<int> v(3);
+	cin >> v[0] >> v[1] >> v[2];
+ 
+	sort(v.begin(), v.end());
+	cout << v[0] << ' ' << v[1] << ' ' << v[2];
+ 
+	return 0;
+}

4bf83150348986a061ed749d53e12fe43a420f9d
s1lv3rrud <86763857+s1lv3rrud@users.noreply.github.com>
2024-08-12 18:28:06 +0900

    [Bronze I] Title: 이제는 더 이상 물러날 곳이 없다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30455. 이제는 더 이상 물러날 곳이 없다/이제는 더 이상 물러날 곳이 없다.cc
@@ -0,0 +1,22 @@
+/*
+
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int n;
+  cin >> n;
+
+  if(n % 2 == 0) {
+    cout << "Duck" << endl;
+  } else {
+    cout << "Goose" << endl;
+  }
+
+}

dfe131f19d9c812179cfbc74186fa11856043b27
s1lv3rrud <86763857+s1lv3rrud@users.noreply.github.com>
2024-09-17 21:20:47 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cc
@@ -0,0 +1,18 @@
+/*
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(int argc, char** argv) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cout << "#  # #### #### #  #" << endl;
+  cout << "#### #  # #  # # #" << endl;
+  cout << "#### #  # #  # # #" << endl;
+  cout << "#  # #### #### #  #" << endl;
+  
+
+  return 0;
+}

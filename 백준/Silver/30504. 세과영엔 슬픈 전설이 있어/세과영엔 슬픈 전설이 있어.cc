71aeae81191fa79707738bd3fe3806e75d02cf72
s1lv3rrud <86763857+s1lv3rrud@users.noreply.github.com>
2024-10-28 21:58:29 +0900

    [Silver IV] Title: 세과영엔 슬픈 전설이 있어, Time: 208 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Silver/30504. 세과영엔 슬픈 전설이 있어/세과영엔 슬픈 전설이 있어.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> A(n), B(n);
+    vector<pair<int, int>> A_with_index(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+        A_with_index[i] = {A[i], i};
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> B[i];
+    }
+
+    // Sort A_with_index based on A values and B in ascending order
+    sort(A_with_index.begin(), A_with_index.end());
+    sort(B.begin(), B.end());
+
+    vector<int> result(n);
+    int j = 0;
+    bool possible = true;
+
+    for (int i = 0; i < n; i++) {
+        while (j < n && B[j] < A_with_index[i].first) {
+            j++;
+        }
+        if (j == n) {
+            possible = false;
+            break;
+        }
+        result[A_with_index[i].second] = B[j];
+        j++;
+    }
+
+    if (possible) {
+        for (int i = 0; i < n; i++) {
+            cout << result[i] << " ";
+        }
+        cout << endl;
+    } else {
+        cout << -1 << endl;
+    }
+
+    return 0;
+}

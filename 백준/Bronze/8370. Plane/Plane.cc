32d95cd995f16d999fe032ead76a858b3eb0af5c
s1lv3rrud <86763857+s1lv3rrud@users.noreply.github.com>
2024-11-01 01:39:40 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n1, n2, k1, k2;
+    cin >> n1 >> k1 >> n2 >> k2;
+
+    cout << n1 * k1 + n2 * k2;
+
+    return 0;
+}
